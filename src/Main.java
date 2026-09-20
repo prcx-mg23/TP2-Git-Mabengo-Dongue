4842170844897d92a94ea964d4e8df75a99368c4
prcx-mg23 <gloireprcx@gmail.com>
2026-09-20 12:35:53 +0200

    Modification du message de Main(version A)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Projet collaboratif - TP2");
+        System.out.println("Projet collaboratif - TP2(Version A)");
     }
 }
